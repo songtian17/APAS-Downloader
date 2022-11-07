670c789c0965cacbb6ba5d9ec52d6eb975a13b8d
Song Tian Liu <44151290+songtian17@users.noreply.github.com>
2022-11-07 11:29:43 +0800

    Update content.js
    Fixed not downloading code with number of lines <= 32

--- content.js
+++ content.js
@@ -9,35 +9,44 @@ async function downloadAPAS() {
         var fileName = document.querySelector(".panel-title").innerText + ".c"
         let contentArr = []
 
-        let scrollBar = document.querySelector("#editor0 > div.ace_scrollbar.ace_scrollbar-v")
-        scrollBar.scrollTop = scrollBar.scrollHeight
-        await sleep(50)
-        let lineNumList = document.getElementsByClassName("ace_gutter-cell")
-        let lastLine = parseInt(lineNumList[lineNumList.length - 1].innerText)
-
-        let oneScroll = document.querySelector("#editor0 > div.ace_scroller > div").clientHeight / (Math.ceil(lastLine / 31) + 1)
-        let prevEnd = 0
-        for (let i = 0; i < scrollBar.scrollHeight; i += oneScroll) {
-            scrollBar.scrollTop = i
-            await sleep(50)
-
-            let lineNumList = document.getElementsByClassName("ace_gutter-cell")
+        if (document.querySelector("#editor0 > div.ace_scrollbar.ace_scrollbar-v").style.display == 'none') {
             let lineList = document.getElementsByClassName("ace_line")
             let maxLength = lineList.length
+            for (let k = 0; k < maxLength; k++) {
+                let lineContent = lineList[k].innerText.replace(/\u00A0/g, " ")
+                contentArr.push(lineContent)
+            }
+        } else {
+            let scrollBar = document.querySelector("#editor0 > div.ace_scrollbar.ace_scrollbar-v")
+            scrollBar.scrollTop = scrollBar.scrollHeight
+            await sleep(50)
+            let lineNumList = document.getElementsByClassName("ace_gutter-cell")
+            let lastLine = parseInt(lineNumList[lineNumList.length - 1].innerText)
 
-            for (let j = 0; j < maxLength; j++) {
-                if (prevEnd + 1 == parseInt(lineNumList[j].innerText)) {
-                    for (let k = j; k < maxLength; k++) {
-                        let lineContent = lineList[k].innerText.replace(/\u00A0/g, " ")
-                        contentArr.push(lineContent)
+            let oneScroll = document.querySelector("#editor0 > div.ace_scroller > div").clientHeight / (Math.ceil(lastLine / 31) + 1)
+            let prevEnd = 0
+            for (let i = 0; i < scrollBar.scrollHeight; i += oneScroll) {
+                scrollBar.scrollTop = i
+                await sleep(50)
+
+                let lineNumList = document.getElementsByClassName("ace_gutter-cell")
+                let lineList = document.getElementsByClassName("ace_line")
+                let maxLength = lineList.length
+
+                for (let j = 0; j < maxLength; j++) {
+                    if (prevEnd + 1 == parseInt(lineNumList[j].innerText)) {
+                        for (let k = j; k < maxLength; k++) {
+                            let lineContent = lineList[k].innerText.replace(/\u00A0/g, " ")
+                            contentArr.push(lineContent)
+                        }
+                        break
                     }
+                }
+
+                prevEnd = parseInt(lineNumList[maxLength - 1].innerText)
+                if (prevEnd == lastLine) {
                     break
                 }
-            }
-
-            prevEnd = parseInt(lineNumList[maxLength - 1].innerText)
-            if (prevEnd == lastLine) {
-                break
             }
         }
 
